47af5213b4b04926391537a68379c20277558a82
liuchonglong <chonglong_liu@odao.com>
2018-08-28 14:07:43 +0800

    springboot + mybatis+ pagehelper + log4j2  初始化demo

---
+++ src/main/java/com/lcl/demo3/Demo3Application.java
@@ -0,0 +1,14 @@
+package com.lcl.demo3;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.lcl.demo3.mapper")
+public class Demo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3Application.class, args);
+	}
+}
---
+++ src/main/java/com/lcl/demo3/util/StringUtils.java
@@ -0,0 +1,20 @@
+package com.lcl.demo3.util;
+
+import java.util.UUID;
+
+/**
+ * @author liuchonglong
+ * @version V1.0
+ * @Description: TODO
+ * @date 2018/8/27 16:26
+ */
+public class StringUtils {
+
+    /**
+     * 获取uuid
+     * @return
+     */
+    public static String getUUID(){
+        return UUID.randomUUID().toString().replace("-", "").toLowerCase();
+    }
+}
